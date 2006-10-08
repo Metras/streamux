0b3d67345d543a894c88b512088413d7d6801206
natulte <natulte@baa99073-d719-0410-821e-03c35ea858c1>
2006-10-08 11:20:44 +0000

    Change the checkout of mpd to an export. We don't need revision control here.

--- build-static-mpd.sh
+++ build-static-mpd.sh
@@ -5,7 +5,7 @@
 
 VERSION="0.12.0"
 
-svn co https://svn.musicpd.org/mpd/tags/release-$VERSION/ mpd
+svn export https://svn.musicpd.org/mpd/tags/release-$VERSION/ mpd
 cd mpd
 ./autogen.sh
 ./configure --enable-static --disable-shared --disable-dependency-tracking --disable-ao --enable-shout --disable-sun --disable-pulse --disable-oss --enable-alsa
