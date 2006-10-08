fe7a258b1d5d25704dab6d9d29b2b4fb0a382871
natulte <natulte@baa99073-d719-0410-821e-03c35ea858c1>
2006-10-08 08:39:48 +0000

    Add a script that builds a static version of mpd, with all input plugins compiled in and alsa+shout outputs compiled in.

---
+++ trunk/build-static-mpd.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# Checkout and build a static version of mpd with all decoders
+# compiled in, and shoutcast and alsa output (for testing)
+
+VERSION="0.12.0"
+
+svn co https://svn.musicpd.org/mpd/tags/release-$VERSION/ mpd
+cd mpd
+./autogen.sh
+./configure --enable-static --disable-shared --disable-dependency-tracking --disable-ao --enable-shout --disable-sun --disable-pulse --disable-oss --enable-alsa
+make LDFLAGS='-all-static'
+mv src/mpd .
+cd ..
