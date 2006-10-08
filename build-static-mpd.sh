5aaed9fb0e31dd6b262fece224f39f3bbeec225d
natulte <natulte@baa99073-d719-0410-821e-03c35ea858c1>
2006-10-08 23:44:40 +0000

    More work on the Streamer component. A functional status() function enables fomr more in-control tracking of the streaming. Also use a hacked MPD to have seamless icecast streaming.

--- build-static-mpd.sh
+++ build-static-mpd.sh
@@ -3,12 +3,19 @@
 # Checkout and build a static version of mpd with all decoders
 # compiled in, and shoutcast and alsa output (for testing)
 
-VERSION="0.12.0"
+# Checkout or update a pristine copy of the MPD trunk
+if [ -d mpd ]; then
+    cd mpd
+    svn revert -R *
+    svn update
+else
+    svn co https://svn.musicpd.org/mpd/trunk/ mpd
+    cd mpd
+fi
 
-svn export https://svn.musicpd.org/mpd/tags/release-$VERSION/ mpd
-cd mpd
-./autogen.sh
-./configure --enable-static --disable-shared --disable-dependency-tracking --disable-ao --enable-shout --disable-sun --disable-pulse --disable-oss --enable-alsa
+# Apply the Streamux machette-shoutcast patch
+patch -p0 < ../mpd-seamless-icecast.patch
+
+./autogen.sh --enable-static --disable-shared --disable-dependency-tracking --disable-ao --enable-shout --disable-sun --disable-pulse --disable-oss --enable-alsa
 make LDFLAGS='-all-static'
-mv src/mpd .
 cd ..
